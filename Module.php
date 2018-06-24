f4b82403afa25fbcaead8b8e40c0c2e93b088b79
Daniel Berthereau <Daniel.github@Berthereau.net>
2018-06-25 00:00:00 +0200

    Added random sort for search of resources.

--- Module.php
+++ Module.php
@@ -2,6 +2,8 @@
 namespace Next;
 
 use Omeka\Module\AbstractModule;
+use Zend\EventManager\Event;
+use Zend\EventManager\SharedEventManagerInterface;
 
 /**
  * Next
@@ -17,5 +19,31 @@ class Module extends AbstractModule
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function attachListeners(SharedEventManagerInterface $sharedEventManager)
+    {
+        $adapters = [
+            \Omeka\Api\Adapter\ItemAdapter::class,
+            \Omeka\Api\Adapter\ItemSetAdapter::class,
+            \Omeka\Api\Adapter\MediaAdapter::class,
+        ];
+        foreach ($adapters as $adapter) {
+            // Add the tagging and tag filters to resource search.
+            $sharedEventManager->attach(
+                $adapter,
+                'api.search.query',
+                [$this, 'apiSearchQuery']
+            );
+        }
+    }
+
+    public function apiSearchQuery(Event $event)
+    {
+        $query = $event->getParam('request')->getContent();
+        if (isset($query['sort_by']) && $query['sort_by'] === 'random') {
+            $qb = $event->getParam('queryBuilder');
+            $qb->orderBy('RAND()');
+        }
     }
 }
