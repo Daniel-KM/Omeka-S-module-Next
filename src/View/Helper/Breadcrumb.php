a1f0d0ad639e2a3d1ad8551dcfb2dd03f27d0328
Daniel Berthereau <Daniel.github@Berthereau.net>
2019-08-05 00:00:00 +0200

    Added an option to skip breadcrumb on homepage.

--- src/View/Helper/Breadcrumb.php
+++ src/View/Helper/Breadcrumb.php
@@ -14,12 +14,15 @@ class Breadcrumb extends AbstractHelper
      *
      * @todo Manage the case when there is no default site.
      * @todo Manage the option current for the pages and convert nav into crumbs.
+     * @todo Manage the case where the home page is not a page and the editor doesn't want breadcrumb on it.
+     * @todo Use the standard Breadcrumbs helper instead of conversion into a partial? It will rights and translator. See \Zend\View\Helper\Navigation\Breadcrumbs
      *
      * @params array $options Managed options:
      * - home (bool) Prepend home (true by default)
      * - prepend (array) A list of crumbs to insert after home
      * - current (bool) Append current resource if any (true by default; always
      *   true for pages currently)
+     * - homepage (bool) Display the breadcrumb on the home page (false by default)
      * - partial (string) The partial to use (default: "common/breadcrumb")
      * Options are passed to the partial too.
      * @return string The html breadcrumb.
@@ -38,6 +41,7 @@ class Breadcrumb extends AbstractHelper
             'home' => true,
             'prepend' => [],
             'current' => true,
+            'homepage' => false,
             'nav' => null,
             'partial' => $this->partial,
         ];
@@ -72,9 +76,13 @@ class Breadcrumb extends AbstractHelper
         }
 
         switch ($matchedRouteName) {
-            // Home page.
+            // Home page, without default site or defined home page.
             case 'top':
             case 'site':
+                if (!$options['homepage']) {
+                    return '';
+                }
+
                 if (!$options['home'] != $options['current']) {
                     $crumbs[] = [
                         'resource' => $site,
@@ -185,7 +193,21 @@ class Breadcrumb extends AbstractHelper
                     ];
                 }
                 break;
+
             case 'site/page':
+                if (!$options['homepage']) {
+                    $homepage = version_compare(\Omeka\Module::VERSION, '1.4', '>=')
+                        ? $site->homepage()
+                        : null;
+                    if (!$homepage) {
+                        $linkedPages = $site->linkedPages();
+                        $homepage = $linkedPages ? current($linkedPages) : null;
+                    }
+                    if ($homepage && $homepage->id() === $vars->page->id()) {
+                        return '';
+                    }
+                }
+
                 $options['nav'] = $site->publicNav();
                 break;
         }
