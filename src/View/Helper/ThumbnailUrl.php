c89aae50ee6425cef7d52f86559278ec782bd600
Daniel Berthereau <Daniel.github@Berthereau.net>
2019-08-26 00:00:00 +0200

    Allowed to get a thumbnail url for a page.

--- src/View/Helper/ThumbnailUrl.php
+++ src/View/Helper/ThumbnailUrl.php
@@ -2,6 +2,8 @@
 namespace Next\View\Helper;
 
 use Omeka\Api\Representation\AbstractRepresentation;
+use Omeka\Api\Representation\AssetRepresentation;
+use Omeka\Api\Representation\SitePageRepresentation;
 use Zend\View\Helper\AbstractHtmlElement;
 
 /**
@@ -22,6 +24,17 @@ class ThumbnailUrl extends AbstractHtmlElement
      */
     public function __invoke(AbstractRepresentation $representation, $type = 'square')
     {
+        if ($representation instanceof SitePageRepresentation) {
+            $representation = $this->thumbnailUrlPage($representation);
+            if (!$representation) {
+                return;
+            }
+        }
+
+        if ($representation instanceof AssetRepresentation) {
+            return $representation->assetUrl();
+        }
+
         if (version_compare(\Omeka\Module::VERSION, '1.3', '>=')) {
             $thumbnail = $representation->thumbnail();
             if ($thumbnail) {
@@ -33,5 +46,81 @@ class ThumbnailUrl extends AbstractHtmlElement
         return $primaryMedia
             ? $primaryMedia->thumbnailUrl($type)
             : null;
+    }
+
+    protected function thumbnailUrlPage(SitePageRepresentation $page)
+    {
+        $view = $this->getView();
+        $api = $view->plugin('api');
+
+        $layoutsWithResource = [
+            // 'html',
+            // Core.
+            'media',
+            'browsePreview',
+            'itemShowcase',
+            'itemShowCase',
+            'itemWithMetadata',
+            // BlockPlus.
+            'assets',
+            'resourceText',
+        ];
+
+        $blocks = $page->blocks();
+        foreach ($blocks as $block) {
+            $layout = $block->layout();
+            if (in_array($layout, $layoutsWithResource)) {
+                switch ($layout) {
+                    case 'media':
+                    case 'itemShowcase':
+                    case 'itemShowCase':
+                    case 'itemWithMetadata':
+                    case 'resourceText':
+                        /** @var \Omeka\Api\Representation\SiteBlockAttachmentRepresentation $attachement */
+                        $attachments = $block->attachments();
+                        if (empty($attachments)) {
+                            break;
+                        }
+                        $attachment = reset($attachments);
+                        return $attachment->media() ?: $attachment->item();
+
+                    case 'browsePreview':
+                        $resourceType = $block->dataValue('resource_type', 'items');
+                        $query = [];
+                        parse_str(ltrim($block->dataValue('query'), '? '), $query);
+                        $site = $block->page()->site();
+                        if ($view->siteSetting('browse_attached_items', false)) {
+                            $query['site_attachments_only'] = true;
+                        }
+                        $query['site_id'] = $site->id();
+                        if (!isset($query['sort_by'])) {
+                            $query['sort_by'] = 'created';
+                        }
+                        if (!isset($query['sort_order'])) {
+                            $query['sort_order'] = 'desc';
+                        }
+                        $representation = $api->searchOne($resourceType, $query)->getContent();
+                        if ($representation) {
+                            return $representation;
+                        }
+                        break;
+
+                    case 'assets':
+                        $assets = $block->dataValue('assets', []);
+                        foreach ($assets as $assetData) {
+                            if (empty($assetData['asset'])) {
+                                continue;
+                            }
+                            try {
+                                /** @var \Omeka\Api\Representation\AssetRepresentation $asset */
+                                $asset = $api->read('assets', $assetData['asset'])->getContent();
+                                return $asset;
+                            } catch (\Omeka\Api\Exception\NotFoundException $e) {
+                            }
+                        }
+                        break;
+                }
+            }
+        }
     }
 }
