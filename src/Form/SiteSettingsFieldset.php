3d2363ff8b2ab8de00a6ee249cd411ce5754507f
Daniel Berthereau <Daniel.git@Berthereau.net>
2021-01-04 00:00:00 +0000

    Removed dead code.

--- src/Form/SiteSettingsFieldset.php
+++ src/Form/SiteSettingsFieldset.php
@@ -25,17 +25,6 @@ class SiteSettingsFieldset extends Fieldset
 17,24 created desc
 73 dcterms:title asc',
                 ],
-            ])
-            ->add([
-                'type' => Element\Checkbox::class,
-                'name' => 'next_search_used_terms',
-                'options' => [
-                    'label' => 'List only used properties and resources classes', // @translate
-                    'info' => 'Restrict the list of properties and resources classes to the used ones in advanced search form (for properties, when option "templates" is not used).', // @translate
-                ],
-                'attributes' => [
-                    'id' => 'next_search_used_terms',
-                ],
             ]);
 
         $this
