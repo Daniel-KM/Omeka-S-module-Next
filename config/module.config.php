5b9fe2db79307668a0f188fc3f5f7a8d244d4c33
Daniel Berthereau <Daniel.git@Berthereau.net>
2026-03-09 00:00:00 +0000

    Removed view helper DefaultSiteSlug for Common DefaultSite.

--- config/module.config.php
+++ config/module.config.php
@@ -9,31 +9,17 @@ return [
         ],
     ],
     'view_manager' => [
-        'template_path_stack' => [
-            dirname(__DIR__) . '/view',
-        ],
         'strategies' => [
             'ViewJsonStrategy',
         ],
     ],
     'view_helpers' => [
         'invokables' => [
-            'defaultSiteSlug' => View\Helper\DefaultSiteSlug::class,
             'itemSetPosition' => View\Helper\ItemSetPosition::class,
         ],
         'factories' => [
             'publicResourceUrl' => Service\ViewHelper\PublicResourceUrlFactory::class,
             'userSiteSlugs' => Service\ViewHelper\UserSiteSlugsFactory::class,
-        ],
-    ],
-    'translator' => [
-        'translation_file_patterns' => [
-            [
-                'type' => 'gettext',
-                'base_dir' => dirname(__DIR__) . '/language',
-                'pattern' => '%s.mo',
-                'text_domain' => null,
-            ],
         ],
     ],
     'next' => [
